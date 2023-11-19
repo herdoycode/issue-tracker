08bdd61ef6ff7639552611f5b6e9cfeee10fc196
= <=>
2023-11-19 21:33:59 +0600

    Create navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Container } from "@radix-ui/themes";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { IoBug } from "react-icons/io5";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+  const navLinks = [
+    { link: "/dashboard", label: "Dashboard" },
+    { link: "/issues", label: "Issues" },
+  ];
+
+  const linkClass = (path: string) => {
+    let classes = "hover:text-zinc-900 transition-colors text-zinc-";
+    return currentPath === path ? (classes += "900") : (classes += "500");
+  };
+
+  return (
+    <Container className="border-b">
+      <nav className="flex items-center gap-4 py-4">
+        <Link className="text-2xl" href="/">
+          <IoBug />
+        </Link>
+        <ul className="flex items-center gap-3">
+          {navLinks.map(({ label, link }) => (
+            <li>
+              <Link className={linkClass(link)} key={label} href={link}>
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </Container>
+  );
+};
+
+export default Navbar;
