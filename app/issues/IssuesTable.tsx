8261616c7a3c5505e6974d6e67b23c3e337173f4
= <=>
2023-11-22 00:36:23 +0600

    Refactore code

---
+++ app/issues/IssuesTable.tsx
@@ -0,0 +1,59 @@
+import { Issue, Status } from "@prisma/client";
+import { Table } from "@radix-ui/themes";
+import Link from "next/link";
+import IssueStatusBadge from "./_components/IssueStatusBadge";
+
+export interface IssueQuery {
+  status: Status;
+  orderBy: keyof Issue;
+  page: string;
+}
+
+interface Props {
+  searchParams: IssueQuery;
+  issues: Issue[];
+}
+
+const IssuesTable = ({ searchParams, issues }: Props) => {
+  return (
+    <Table.Root variant="surface">
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell>
+              <Link
+                href={{ query: { ...searchParams, orderBy: column.value } }}
+              >
+                {column.label}
+              </Link>
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {issues.map((issue) => (
+          <Table.Row key={issue.id}>
+            <Table.RowHeaderCell>
+              <Link href={`/issues/${issue.id}`}> {issue.title} </Link>
+            </Table.RowHeaderCell>
+            <Table.Cell>
+              <IssueStatusBadge status={issue.status} />
+            </Table.Cell>
+            <Table.Cell> {issue.createdAt.toDateString()} </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+export default IssuesTable;
+
+export const columns: { label: string; value: keyof Issue }[] = [
+  { label: "Title", value: "title" },
+  { label: "Status", value: "status" },
+  { label: "Date", value: "createdAt" },
+];
+
+export const columnNames = columns.map((column) => column.value);
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,82 +1,42 @@
 import prisma from "@/prisma/client";
-import { Issue, Status } from "@prisma/client";
-import { Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Status } from "@prisma/client";
 import Pagination from "../components/Pagination";
 import IssueActions from "./IssueActions";
-import IssueStatusBadge from "./_components/IssueStatusBadge";
+import IssuesTable, { IssueQuery, columnNames } from "./IssuesTable";
 
 interface Props {
-  searchParams: { status: Status; page: string; orderBy: keyof Issue };
+  searchParams: IssueQuery;
 }
 
 const Issues = async ({ searchParams }: Props) => {
-  const columns: { label: string; value: keyof Issue }[] = [
-    { label: "Title", value: "title" },
-    { label: "Status", value: "status" },
-    { label: "Date", value: "createdAt" },
-  ];
-
   const pageSize = 5;
   const page = parseInt(searchParams.page) || 1;
 
-  const status = Object.values(Status).includes(searchParams.status)
+  const statuses = Object.values(Status);
+
+  const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
-  const orderBy = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const where = { status };
+
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
+    where,
     orderBy,
     skip: (page - 1) * pageSize,
     take: pageSize,
   });
 
-  const issuesCount = await prisma.issue.count({
-    where: {
-      status,
-    },
-  });
+  const issuesCount = await prisma.issue.count({ where });
 
   return (
     <main>
       <IssueActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell>
-                <Link
-                  href={{ query: { ...searchParams, orderBy: column.value } }}
-                >
-                  {column.label}
-                </Link>
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.RowHeaderCell>
-                <Link href={`/issues/${issue.id}`}> {issue.title} </Link>
-              </Table.RowHeaderCell>
-              <Table.Cell>
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell> {issue.createdAt.toDateString()} </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssuesTable issues={issues} searchParams={searchParams} />
       <Pagination
         pageSize={pageSize}
         itemsCount={issuesCount}
