0270da2ea9c6f1aa8d68b54023b14c6eb9ebc07f
= <=>
2024-02-20 11:38:28 +0600

    Build the issue table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,31 @@
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
 const Issues = async () => {
-  return <div>Issues</div>;
+  const issues = await prisma.issue.findMany();
+
+  return (
+    <main>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.RowHeaderCell> {issue.title} </Table.RowHeaderCell>
+              <Table.Cell> {issue.status} </Table.Cell>
+              <Table.Cell> {issue.createdAt.toDateString()} </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </main>
+  );
 };
 
 export default Issues;
