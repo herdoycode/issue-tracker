306c79e541c128b76cabf3693471b5a7d39a63a5
= <=>
2024-02-20 12:28:55 +0600

    Improve Ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -5,25 +5,36 @@ const Issues = async () => {
   const issues = await prisma.issue.findMany();
 
   return (
-    <main>
+    <main className="px-2 md:px-0">
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
-              <Table.RowHeaderCell> {issue.title} </Table.RowHeaderCell>
-              <Table.Cell>
-                {" "}
-                <IssueStatusBadge status={issue.status} />{" "}
+              <Table.RowHeaderCell>
+                {issue.title}
+                <div className="block md:hidden mt-1">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.RowHeaderCell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
-              <Table.Cell> {issue.createdAt.toDateString()} </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {" "}
+                {issue.createdAt.toDateString()}{" "}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
